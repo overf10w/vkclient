e754b9fc17da9161d3af0f0fcf8cdc0e2c497245
pwnzr <pvvner@yandex.ru>
2017-05-15 03:21:50 +0300

    Add & use routes and views(and view engines)

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var index = require('./routes/index');
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
 
 // set up view engine
-app.use(express.static(__dirname + '/views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Use routes
+app.use('/', index);
